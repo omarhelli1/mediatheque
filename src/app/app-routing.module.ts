1cee68c9e90de16501751a74307fdda6d2c03590
HELLI Omar <omarhelli@gmail.com>
2022-06-14 16:30:45 +0200

    feat : creation de footer et nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagesComponent } from './pages/home-page/home-pages.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home', component:HomePagesComponent},
+  {path:'products', component:ProductPageComponent},
+  {path:'**',component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
