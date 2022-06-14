6dddfc8571ba7e57151a20b592bfaae217069020
HELLI Omar <omarhelli@gmail.com>
2022-06-14 14:29:46 +0200

    feat : creation de projet mediateque

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
+import { HomePagesComponent } from './pages/home-page/home-pages.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    HomePagesComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
